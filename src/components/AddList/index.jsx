61679d0e3f8ea4be86a66b98bb764c8193944c55
VERS_41_2 <VERS_41_2@FED.COM.UA>
2020-07-13 04:59:57 +0300

    2.2.

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import List from '../List';
+import Badge from '../Badge';
+import closeSvg from '..//..//assets/img/close.svg'
 
 import './AddButtonList.scss';
 
 const AddList = ({ colors }) => {
-    const [visiblePopup, setVisiblePopup] = useState(true);
+    const [visiblePopup, setVisiblePopup] = useState(false);
+    const [selectedColor, selectColor] = useState(colors[0].id);
 
     return (
         <div className="add-list">
@@ -40,12 +43,21 @@ const AddList = ({ colors }) => {
                 ]}
             />
             {visiblePopup && <div className="add-list__popup">
+                <img
+                onClick={() => {
+                    setVisiblePopup(false)
+                }}
+                src={closeSvg} alt="close button" className="add-list__popup-close-btn"/>
                 <input className="field" type="text" placeholder="Название папки" />
-                <div className="ass-list__popup-colors">
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
+                <div className="add-list__popup-colors">
+                    {colors.map(color => (
+                        <Badge
+                            onClick={() => selectColor(color.id)}
+                            key={color.id}
+                            color={color.name}
+                            className={selectedColor  === color.id && 'active'}
+                            />
+                    ))}
                 </div>
                 <button className="button">Добавить</button>
             </div>}
